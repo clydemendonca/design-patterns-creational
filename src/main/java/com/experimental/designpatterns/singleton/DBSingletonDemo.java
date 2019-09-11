4c50855d1be1b685baad3b925253316d09a83c66
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-09-11 22:16:52 +0530

    Example of singleton

--- src/main/java/com/experimental/designpatterns/singleton/DBSingletonDemo.java
+++ src/main/java/com/experimental/designpatterns/singleton/DBSingletonDemo.java
@@ -1,13 +1,42 @@
 package com.experimental.designpatterns.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBSingletonDemo {
 
     public static void main(String[] args) {
         DBSingleton instance = DBSingleton.getInstance();
+
+        long timeBefore = 0;
+        long timeAfter = 0;
+
         System.out.println(instance);
 
-        DBSingleton anotherInstance = DBSingleton.getInstance();
-        System.out.println(anotherInstance);
+        timeBefore = System.currentTimeMillis();
+        Connection connection = instance.getConnection();
+        timeAfter = System.currentTimeMillis();
+
+        System.out.println(timeAfter - timeBefore);
+
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            int count = statement.executeUpdate("CREATE TABLE Address(ID INT, StreetName VARCHAR(20), "
+                    + "City VARCHAR(20))");
+            System.out.println("Table created.");
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        timeBefore = System.currentTimeMillis();
+        connection = instance.getConnection();
+        timeAfter = System.currentTimeMillis();
+
+        System.out.println(timeAfter - timeBefore);
+
     }
 
 }
