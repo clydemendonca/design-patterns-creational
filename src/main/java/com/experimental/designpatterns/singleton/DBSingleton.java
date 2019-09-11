7c81ef2d6c5b9f1fad703e0367c78198784d5f3c
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-09-11 19:22:28 +0530

    Thread-safety added

--- src/main/java/com/experimental/designpatterns/singleton/DBSingleton.java
+++ src/main/java/com/experimental/designpatterns/singleton/DBSingleton.java
@@ -2,15 +2,23 @@ package com.experimental.designpatterns.singleton;
 
 public class DBSingleton {
 
-    private static DBSingleton instance = null;
+    // volatile makes it Thread-safe
+    private static volatile DBSingleton instance = null;
 
     private DBSingleton() {
+        if(instance != null) {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
     }
 
     public static DBSingleton getInstance() {
         // LAZY LOADING
         if(instance == null) {
-            instance = new DBSingleton();
+            synchronized (DBSingleton.class) {
+                if(instance == null) {
+                    instance = new DBSingleton();
+                }
+            }
         }
         return instance;
     }
