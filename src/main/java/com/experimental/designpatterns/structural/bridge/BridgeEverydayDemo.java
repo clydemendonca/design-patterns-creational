630f6600f103d30abc6314bc43ed00f1512e6aba
Clyde Mendonca <clyde.mendonca01@gmail.com>
2019-09-15 10:11:16 +0530

    Bridge everyday demo

---
+++ src/main/java/com/experimental/designpatterns/structural/bridge/BridgeEverydayDemo.java
@@ -0,0 +1,31 @@
+package com.experimental.designpatterns.structural.bridge;
+
+import org.apache.derby.jdbc.EmbeddedDriver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BridgeEverydayDemo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            DriverManager.registerDriver(new EmbeddedDriver());
+            String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+            Connection connection = DriverManager.getConnection(dbUrl);
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate("CREATE TABLE Address(ID INT, StreetName VARCHAR(20), City VARCHAR(20))");
+
+            System.out.println("Table created.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
